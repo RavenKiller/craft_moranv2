657d2e92eb704d0cc7d2c946710fca8258272169
He Zongtao <raven025@outlook.com>
2020-03-17 18:22:21 +0800

    Initialize

---
+++ file_utils.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+import cv2
+import imgproc
+import math
+import matplotlib.pyplot as plt
+# borrowed from https://github.com/lengstrom/fast-style-transfer/blob/master/src/utils.py
+def get_files(img_dir):
+    imgs, masks, xmls = list_files(img_dir)
+    return imgs, masks, xmls
+
+def list_files(in_path):
+    img_files = []
+    mask_files = []
+    gt_files = []
+    for (dirpath, dirnames, filenames) in os.walk(in_path):
+        for file in filenames:
+            filename, ext = os.path.splitext(file)
+            ext = str.lower(ext)
+            if ext == '.jpg' or ext == '.jpeg' or ext == '.gif' or ext == '.png' or ext == '.pgm':
+                img_files.append(os.path.join(dirpath, file))
+            elif ext == '.bmp':
+                mask_files.append(os.path.join(dirpath, file))
+            elif ext == '.xml' or ext == '.gt' or ext == '.txt':
+                gt_files.append(os.path.join(dirpath, file))
+            elif ext == '.zip':
+                continue
+    # img_files.sort()
+    # mask_files.sort()
+    # gt_files.sort()
+    return img_files, mask_files, gt_files
+
+def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=None):
+        """ save text detection result one by one
+        Args:
+            img_file (str): image file name
+            img (array): raw image context
+            boxes (array): array of result file
+                Shape: [num_detections, 4] for BB output / [num_detections, 4] for QUAD output
+        Return:
+            None
+        """
+        img = np.array(img)
+
+        # make result file list
+        filename, file_ext = os.path.splitext(os.path.basename(img_file))
+
+        # result directory
+        res_file = dirname + "/res_" + filename + '.txt'
+        res_img_file = dirname + "/res_" + filename + '.jpg'
+        
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+
+        with open(res_file, 'w') as f:
+            for i, box in enumerate(boxes):
+                poly = np.array(box).astype(np.int32).reshape((-1))
+                strResult = ','.join([str(p) for p in poly]) + '\r\n'
+                f.write(strResult)
+
+                poly = poly.reshape(-1, 2)
+                cv2.polylines(img, [poly.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)
+                ptColor = (0, 255, 255)
+                if verticals is not None:
+                    if verticals[i]:
+                        ptColor = (255, 0, 0)
+
+                if texts is not None:
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_scale = 1.5
+                    cv2.putText(img, "{}".format(texts[i]), (poly[0][0]+1, poly[0][1]+1), font, font_scale, (0, 0, 0), thickness=1)
+                    cv2.putText(img, "{}".format(texts[i]), tuple(poly[0]), font, font_scale, (0, 255, 255), thickness=1)
+
+        # Save result image
+        cv2.imwrite(res_img_file, img)
+
+def saveSplitTextRects(img,rot_rects,bboxes,save_folder="result",save_file=True,save_prefix="rect",is_save=True):
+    loose = 5 # add more pixels into the rectangle
+    img_cuts = []
+    for k,rectangle in enumerate(rot_rects):
+        cx = rectangle[0][0]
+        cy = rectangle[0][1]
+        width = rectangle[1][0]
+        height = rectangle[1][1]
+
+        angle = rectangle[2]
+        (h,w) = img.shape[:2]
+        
+        #设置旋转矩阵
+        M = cv2.getRotationMatrix2D((w/2,h/2),angle,1.0)
+        cos = np.abs(M[0,0])
+        sin = np.abs(M[0,1])
+        
+        # 计算图像旋转后的新边界
+        nW = int((h*sin)+(w*cos))
+        nH = int((h*cos)+(w*sin))
+        # 调整旋转矩阵的移动距离（t_{x}, t_{y}）
+        M[0,2] += (nW/2) - w/2
+        M[1,2] += (nH/2) - h/2
+        
+        img_rotated = cv2.warpAffine(img,M,(nW,nH))
+
+        res = np.dot(M, np.array([[cx], [cy], [1]]))
+        cx = int(res[0,0])
+        cy = int(res[1,0])
+        x1 = int(cx-width/2)
+        y1 = int(cy-height/2)
+        x2 = int(cx+width/2)
+        y2 = int(cy-height/2)
+        x3 = int(cx+width/2)
+        y3 = int(cy+height/2)
+        x4 = int(cx-width/2)
+        y4 = int(cy+height/2)
+        img_cut = img_rotated[max(0,y1-loose):min(nH,y3+loose),max(0,x1-loose):min(nW,x3+loose),:]
+        m,n,_ = img_cut.shape
+        if m>n+10:
+            img_cut = img_cut.transpose(1,0,2)
+            img_cut = img_cut[::-1,:,:]
+        img_cuts.append(img_cut)
+        if save_file:
+            cv2.imwrite(os.path.join(save_folder,save_prefix+"_"+str(k)+".jpg"),img_cut[:,:,::-1])
+    return img_cuts
+
---
+++ main.py
@@ -0,0 +1,239 @@
+"""  
+Copyright (c) 2019-present NAVER Corp.
+MIT License
+"""
+
+# -*- coding: utf-8 -*-
+import sys
+import os
+import time
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+
+from PIL import Image
+
+import cv2
+from skimage import io
+import numpy as np
+import craft_utils
+import imgproc
+import file_utils
+import json
+import zipfile
+
+from craft import CRAFT
+import tools.utils as utils
+import tools.dataset as dataset
+from models.moran import MORAN
+
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+    
+def craft_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
+    t0 = time.time()
+
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, args.canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=args.mag_ratio)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # refine link
+    if refine_net is not None:
+        with torch.no_grad():
+            y_refiner = refine_net(y, feature)
+        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+    tmp1 = score_link.copy()
+    tmp2 = score_text.copy()
+    t0 = time.time() - t0
+    t1 = time.time()
+
+    # Post-processing
+    boxes, polys, rot_rects = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+    # coordinate adjustment
+    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    rot_rects = craft_utils.adjustResultCoordinatesNew(rot_rects, ratio_w, ratio_h)
+    for k in range(len(polys)):
+        if polys[k] is None: 
+            polys[k] = boxes[k]
+
+    t1 = time.time() - t1
+
+    # render results (optional)
+    render_img = score_text.copy()
+    render_img = np.hstack((render_img, score_link))
+    ret_score_text = imgproc.cvt2HeatmapImg(render_img)
+
+    if args.show_time : print("\ninfer/postproc time : {:.3f}/{:.3f}".format(t0, t1))
+
+    return boxes, polys, ret_score_text,rot_rects
+
+tmp_folder = "new"
+if not os.path.isdir(tmp_folder):
+    os.mkdir(tmp_folder)
+parser = argparse.ArgumentParser(description='CRAFT Text Detection')
+# CRAFT args
+parser.add_argument('--craft_trained_model', default='pretrained/craft_mlt_25k.pth', type=str, help='pretrained model')
+parser.add_argument('--img_path', default='test/1.jpg', type=str, help='folder path to input images')
+parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
+parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
+parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
+parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda for inference')
+parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
+parser.add_argument('--mag_ratio', default=1.5, type=float, help='image magnification ratio')
+parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
+parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
+parser.add_argument('--refine', default=True, action='store_true', help='enable link refiner')
+parser.add_argument('--refiner_model', default='pretrained/craft_refiner_CTW1500.pth', type=str, help='pretrained refiner model')
+# moran 
+
+parser.add_argument('--moran_path', default='pretrained/moran.pth', type=str, help='pretrained moran model')
+args = parser.parse_args()
+moran_path = args.moran_path
+alphabet = '0:1:2:3:4:5:6:7:8:9:a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:$'
+
+if __name__ == '__main__':
+    all_text = []
+    all_text_reverse = []
+    ################################################
+    # CRAFT loading part
+    ################################################
+    # load net
+    net = CRAFT()     # initialize
+    if args.cuda:
+        net.load_state_dict(copyStateDict(torch.load(args.craft_trained_model)))
+    else:
+        net.load_state_dict(copyStateDict(torch.load(args.craft_trained_model, map_location='cpu')))
+
+    if args.cuda:
+        net = net.cuda()
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = False
+
+    net.eval()
+    # LinkRefiner
+    refine_net = None
+    if args.refine:
+        from refinenet import RefineNet
+        refine_net = RefineNet()
+        print('Loading weights of refiner from checkpoint (' + args.refiner_model + ')')
+        if args.cuda:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model)))
+            refine_net = refine_net.cuda()
+            refine_net = torch.nn.DataParallel(refine_net)
+        else:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model, map_location='cpu')))
+
+        refine_net.eval()
+        args.poly = True
+    ################################################
+    # MORAN loading part
+    ################################################
+    cuda_flag = False
+    if torch.cuda.is_available():
+        cuda_flag = True
+        MORAN = MORAN(1, len(alphabet.split(':')), 256, 32, 100, BidirDecoder=True, CUDA=cuda_flag)
+        MORAN = MORAN.cuda()
+    else:
+        MORAN = MORAN(1, len(alphabet.split(':')), 256, 32, 100, BidirDecoder=True, inputDataType='torch.FloatTensor', CUDA=cuda_flag)
+
+    print('loading pretrained model from %s' % moran_path)
+    if cuda_flag:
+        state_dict = torch.load(moran_path)
+    else:
+        state_dict = torch.load(moran_path, map_location='cpu')
+    MORAN_state_dict_rename = OrderedDict()
+    for k, v in state_dict.items():
+        name = k.replace("module.", "") # remove `module.`
+        MORAN_state_dict_rename[name] = v
+    MORAN.load_state_dict(MORAN_state_dict_rename)
+
+    for p in MORAN.parameters():
+        p.requires_grad = False
+    MORAN.eval()
+
+    ################################################
+    # CRAFT processing part
+    ################################################
+    # load data
+    image = imgproc.loadImage(args.img_path)
+    image_raw = image.copy()
+
+    bboxes, polys, score_text,rot_rects = craft_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
+
+    # save text rectangles
+    filename, file_ext = os.path.splitext(os.path.basename(args.img_path))
+    img_cuts = file_utils.saveSplitTextRects(image,rot_rects,bboxes,save_folder=tmp_folder,save_file=False,save_prefix="rect_"+filename)
+
+    ################################################
+    # MORAN processing part
+    ################################################
+    for v in os.listdir(tmp_folder):
+        img_path = os.path.join(tmp_folder,v)
+        if not ('.jpg' in img_path or '.png' in img_path):
+            continue
+        converter = utils.strLabelConverterForAttention(alphabet, ':')
+        transformer = dataset.resizeNormalize((100, 32))
+        image = Image.open(img_path).convert('L')
+        image = transformer(image)
+        # os.remove(img_path)
+
+        if cuda_flag:
+            image = image.cuda()
+        image = image.view(1, *image.size())
+        image = Variable(image)
+        text = torch.LongTensor(1 * 5)
+        length = torch.IntTensor(1)
+        text = Variable(text)
+        length = Variable(length)
+
+        max_iter = 20
+        t, l = converter.encode('0'*max_iter)
+        utils.loadData(text, t)
+        utils.loadData(length, l)
+        output = MORAN(image, length, text, text, test=True, debug=False)
+
+        preds, preds_reverse = output[0]
+        _, preds = preds.max(1)
+        _, preds_reverse = preds_reverse.max(1)
+
+        sim_preds = converter.decode(preds.data, length.data)
+        sim_preds = sim_preds.strip().split('$')[0]
+        sim_preds_reverse = converter.decode(preds_reverse.data, length.data)
+        sim_preds_reverse = sim_preds_reverse.strip().split('$')[0]
+        all_text.append(sim_preds)
+        all_text_reverse.append(sim_preds_reverse[::-1])
+        # print('\nResult:\n' + 'Left to Right: ' + sim_preds + '\nRight to Left: ' + sim_preds_reverse + '\n\n')
+    file_utils.saveResult(args.img_path, image_raw[:,:,::-1], polys, texts=all_text,dirname=tmp_folder)
+    print(all_text)
+    print(all_text_reverse)
