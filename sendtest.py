ac7e963b40060e1331d06ee37f877d3ed2502cce
He Zongtao <raven025@outlook.com>
2020-09-28 21:49:12 +0800

    succenssfully send image via HTTP and save it

--- sendtest.py
+++ sendtest.py
@@ -3,14 +3,32 @@ import numpy as np
 import base64
 from naoqi import ALProxy
 import vision_definitions
+import time
+import json
 
 ip = "127.0.0.1"
 port = 9559
+remote_url = "http://192.168.1.119:5678/text_recognition"
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+fps = 1
 avd = ALProxy("ALVideoDevice",ip,port)
-cam_handler = avd.subscribeCamera(vision_definitions.kTopCamera,vision_definitions.kVGA,vision_definitions.kRGBColorSpace,10)
-
-img = avd.getImageLocal(cam_handler)
-print(img)
-
-
-# print(res)
+mem = ALProxy("ALMemory",ip,port)
+avd.setActiveCamera(vision_definitions.kTopCamera)
+avd.startCamera(vision_definitions.kTopCamera)
+avd.openCamera(vision_definitions.kTopCamera)
+cam_handler = avd.subscribe("mycam",vision_definitions.kVGA,vision_definitions.kRGBColorSpace,fps)
+print(avd.getSubscribers())
+print('================')
+try:
+    while True:
+        img = avd.getImageRemote(cam_handler)
+        print(img[0])
+        if img:
+            print("sending")
+            print(len(img[6]))
+            print(img[6][0:10])
+            res = requests.post(remote_url,headers=headers, data={"image":base64.b64encode(img[6])})
+            avd.releaseImage(cam_handler)
+        time.sleep(1/fps)
+except:
+    avd.unsubscribe(cam_handler)
--- server.py
+++ server.py
@@ -1,14 +1,33 @@
 import tornado.ioloop
 import tornado.web
 import base64
-
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import time
+fig = plt.figure()
+ax = fig.add_axes()
 class TextRecognitionHanlder(tornado.web.RequestHandler):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fig=plt.figure()
+        self.ax = self.fig.add_axes([0.1,0.1,0.9,0.9])
     def post(self):
-        print(self.get_argument('image'))
+        zz = self.get_argument('image')
+
+        print("received")
+        img = np.array(list(base64.b64decode(self.get_argument('image'))),dtype=np.uint8)
+        img = img.reshape((480,640,3))
+        self.ax.imshow(img)
+        plt.savefig("test.png")
+        print(img.shape)
+        print(img.dtype)
+
 
 def make_app():
     return tornado.web.Application([
-        (r"/test_recognition", TextRecognitionHanlder),
+        (r"/text_recognition", TextRecognitionHanlder),
     ])
 
 if __name__ == "__main__":
